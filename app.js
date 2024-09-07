ddf576785124073e4ea481eea0b9246a3b6360be
jose <josemlaguadoc@gmail.com>
2024-09-06 19:08:15 -0500

    proyecto ley de emprendimiento

---
+++ app.js
@@ -0,0 +1,54 @@
+document.getElementById('toggleEje1Btn').addEventListener('click', function() {
+    var info = document.getElementById('extraEje1Info');
+    if (info.classList.contains('hidden')) {
+        info.classList.remove('hidden');
+        this.textContent = 'Ocultar información';
+    } else {
+        info.classList.add('hidden');
+        this.textContent = 'Mostrar más información';
+    }
+});
+
+document.getElementById('toggleEje2Btn').addEventListener('click', function() {
+    var info = document.getElementById('extraEje2Info');
+    if (info.classList.contains('hidden')) {
+        info.classList.remove('hidden');
+        this.textContent = 'Ocultar información';
+    } else {
+        info.classList.add('hidden');
+        this.textContent = 'Mostrar más información';
+    }
+});
+
+document.getElementById('toggleEje3Btn').addEventListener('click', function() {
+    var info = document.getElementById('extraEje3Info');
+    if (info.classList.contains('hidden')) {
+        info.classList.remove('hidden');
+        this.textContent = 'Ocultar información';
+    } else {
+        info.classList.add('hidden');
+        this.textContent = 'Mostrar más información';
+    }
+});
+
+document.getElementById('toggleEje4Btn').addEventListener('click', function() {
+    var info = document.getElementById('extraEje4Info');
+    if (info.classList.contains('hidden')) {
+        info.classList.remove('hidden');
+        this.textContent = 'Ocultar información';
+    } else {
+        info.classList.add('hidden');
+        this.textContent = 'Mostrar más información';
+    }
+});
+
+document.getElementById('toggleEje5Btn').addEventListener('click', function() {
+    var info = document.getElementById('extraEje5Info');
+    if (info.classList.contains('hidden')) {
+        info.classList.remove('hidden');
+        this.textContent = 'Ocultar información';
+    } else {
+        info.classList.add('hidden');
+        this.textContent = 'Mostrar más información';
+    }
+});
